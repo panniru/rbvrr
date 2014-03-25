8cbf51dc1d3af996c3640300b80b064901acabbb
Priya14 <shanmugapriya141292@gmail.com>
2014-03-25 18:15:05 +0530

    second commit

--- Gemfile
+++ Gemfile
@@ -4,11 +4,13 @@ source 'https://rubygems.org'
 gem 'rails', '4.0.2'
 
 # Use sqlite3 as the database for Active Record
-#gem 'sqlite3'
 
-gem 'devise', '~> 3.2.4'
+
+
 # Use SCSS for stylesheets
-gem 'sass-rails', '~> 4.0.0'
+
+gem 'sass-rails', '~> 4.0.2'
+gem 'sass', '~> 3.2.0'
 
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
@@ -17,7 +19,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
 gem "bootstrap-will_paginate", "~> 0.0.10"
 gem 'bootstrap-sass', '~>3.1.1'
-gem 'haml'
+gem 'haml', '~> 4.0.5'
 
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
@@ -36,9 +38,11 @@ group :doc do
   gem 'sdoc', require: false
 end
 group :development, :test do
-  gem 'pg'
+ gem 'sqlite3'
 end
-
+group :production do
+gem 'pg'
+end
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.1.2'
 
--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140321083536) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "models", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -31,8 +28,8 @@ ActiveRecord::Schema.define(version: 20140321083536) do
     t.datetime "updated_at"
   end
 
-  add_index "models", ["email"], name: "index_models_on_email", unique: true, using: :btree
-  add_index "models", ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true, using: :btree
+  add_index "models", ["email"], name: "index_models_on_email", unique: true
+  add_index "models", ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
 
   create_table "users", force: true do |t|
     t.string   "username"
